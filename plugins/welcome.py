495d974016b440c939ac8d8931081e56470a7727
ZRunner <z.runner.mc@gmail.com>
2020-09-01 21:56:53 +0200

    beautifulying

--- plugins/welcome.py
+++ plugins/welcome.py
@@ -14,13 +14,14 @@ class Welcome(commands.Cog):
         """Called when a member joins a guild"""
         g = member.guild
         config = self.bot.server_configs[g.id]
-        rolesID =  config["welcome_roles"]
+        rolesID = config["welcome_roles"]
         if not rolesID:  # si rien n'a été configuré
             return
         roles = [g.get_role(x) for x in rolesID]
         pos = g.me.top_role.position
         roles = filter(lambda x: (x is not None) and (x.position < pos), roles)
         await member.add_roles(*roles, reason="New members roles")
+
 
 def setup(bot):
     bot.add_cog(Welcome(bot))
