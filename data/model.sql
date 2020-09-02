2bd7a050707030d2250a752ea6d39703930e52d0
ZRunner <z.runner.mc@gmail.com>
2020-09-02 14:46:40 +0200

    nouvelle db pour les salons de contact

--- data/model.sql
+++ data/model.sql
@@ -12,3 +12,11 @@ CREATE TABLE IF NOT EXISTS `thanks` (
   `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 CREATE INDEX IF NOT EXISTS idx_thanks_guild ON `thanks` (`guild`);
+
+CREATE TABLE IF NOT EXISTS `contact_channels` (
+  `guild` BIGINT NOT NULL,
+  `channel` BIGINT PRIMARY KEY NOT NULL,
+  `author` BIGINT NOT NULL,
+  `timestamp` TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
+CREATE INDEX IF NOT EXISTS idx_contactchannels_guild ON `contact_channels` (`guild`);
