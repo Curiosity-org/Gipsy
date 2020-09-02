a3618d0fe537a57b232d689e9d091a1514daa35c
ZRunner <z.runner.mc@gmail.com>
2020-09-02 15:36:08 +0200

    perms admin uwu

--- checks.py
+++ checks.py
@@ -2,4 +2,4 @@ def is_bot_admin(ctx):
     return ctx.author.id in ctx.bot.config['bot_admins']
 
 def is_admin(ctx):
-    return ctx.guild is None or ctx.author.guild_permissions.administrator
+    return ctx.guild is None or ctx.author.guild_permissions.administrator or is_bot_admin(ctx)
