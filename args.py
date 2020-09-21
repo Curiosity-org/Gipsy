c3d089e8bbce349186e6c12890b015050b5633a4
Z Runner <z.runner.mc@gmail.com>
2020-09-21 08:58:56 +0000

    Update args.py

--- args.py
+++ args.py
@@ -11,13 +11,13 @@ class tempdelta(commands.Converter):
         d = 0
         found = False
         # ctx.invoked_with
-        for x in [('y', 86400*365), ('w', 604800), ('d', 86400), ('h', 3600), ('m', 60)]:
+        for x in [('y', 86400*365), ('w', 604800), ('d', 86400), ('h', 3600), ('m', 60), ('min', 60)]:
             r = re.search(r'^(\d+)'+x[0]+'$', argument)
-            if r != None:
+            if r is not None:
                 d += int(r.group(1))*x[1]
                 found = True
         r = re.search(r'^(\d+)h(\d+)m?$', argument)
-        if r != None:
+        if r is not None:
             d += int(r.group(1))*3600 + int(r.group(2))*60
             found = True
         if not found:
