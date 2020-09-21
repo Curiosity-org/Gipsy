1286717500c22975787827146827b1bf34aa71ff
Gamer4life <lpuvilla0001@mymail.lausd.net>
2020-09-20 17:02:57 -0700

    Changer le message

--- checks.py
+++ checks.py
@@ -7,7 +7,7 @@ async def is_admin(ctx: commands.Context):
     admin = ctx.guild is None or ctx.author.guild_permissions.administrator or is_bot_admin(ctx)
     if not admin:
         try:
-            await ctx.send("Il vous manque la permission 'Administrateur' pour faire cela")
+            await ctx.send("Il vous manque la permission 'Administrateur' sur le serveur pour faire cela/vous n'etes pas un administrateur du bot")
         except discord.errors.Forbidden:
             pass
     return admin
