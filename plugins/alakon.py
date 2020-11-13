fbc81aa63034635bb17226e57f4c97becbbdf6c0
Leirof <vince.lrf@gmail.com>
2020-11-13 13:42:09 +0100

    /cookie 7: evolution

--- plugins/alakon.py
+++ plugins/alakon.py
@@ -11,10 +11,14 @@ class Alakon(commands.Cog):
     @commands.command(name="cookie")
     async def cookie(self, ctx: commands.Context, *, user: discord.User = None):
         """La fonction la plus complexe du bot: donne un cookie à l'utilisateur qui en demande."""
+
         if user:
-            await ctx.send(f"Voilà pour vous {user.mention}: :cookie:"
+            message = f"Voilà pour vous {user.mention}: :cookie:"
         else:
-            await ctx.send(f"Voilà pour vous {ctx.author.mention}: :cookie: TEST"
+            message = f"Voilà pour vou {ctx.author.mention} :cookie:"
+        webhook = ctx.channel.create_webhook(*,"Villager number 6", avatar="https://d31sxl6qgne2yj.cloudfront.net/wordpress/wp-content/uploads/20190121140737/Minecraft-Villager-Head.jpg")
+        await webhook.send(message)
+        await ctx.message.delete()
 
 
 def setup(bot):
