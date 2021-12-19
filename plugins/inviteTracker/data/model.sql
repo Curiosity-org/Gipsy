0ac8fae439a5e5b27dd813af108a1cfb842672b6
Z Runner <z.runner.mc@gmail.com>
2021-12-19 11:51:53 +0100

    my bad I forgot smth

--- plugins/inviteTracker/data/model.sql
+++ plugins/inviteTracker/data/model.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS `invites` (
-    `guild` BIGINT NOT NULL, 
+    `guild` BIGINT, 
     `channel` BIGINT NOT NULL,
-    `user` BIGINT NOT NULL, /*The people who created the invite*/
+    `user` BIGINT, /*The people who created the invite*/
     `id` BIGINT NOT NULL,
     `code` TINYTEXT NOT NULL,
     `uses` INT NOT NULL, /*Needed to detect an invitation use*/
