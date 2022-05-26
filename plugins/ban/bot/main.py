03649133e5408c74f29b09cad3f8eb5107055de4
Aeris One <aeris@aeris-one.fr>
2022-05-26 14:44:10 +0200

    Refactor

--- plugins/ban/bot/main.py
+++ plugins/ban/bot/main.py
@@ -1,7 +1,7 @@
 import random
 import nextcord
 from nextcord.ext import commands
-from utils import Gunibot, MyContext  
+from utils import Gunibot, MyContext
 
 
 class Ban(commands.Cog):
@@ -9,11 +9,10 @@ class Ban(commands.Cog):
     def __init__(self, bot: Gunibot):
         self.bot = bot
         self.file = "ban"
- 
- 
-    #------------------#
+
+    # ------------------#
     # Commande /ban    #
-    #------------------#
+    # ------------------#
 
     @commands.command(name="ban")
     @commands.guild_only()
@@ -31,7 +30,8 @@ class Ban(commands.Cog):
             return
         if ctx.guild.id != "125723125685026816" and ctx.guild.id != "689159304049197131":
             try:
-                await ctx.guild.ban(user, delete_message_days=0, reason=f"Banned by {ctx.author} ({ctx.author.id}). Reason : {reason}")
+                await ctx.guild.ban(user, delete_message_days=0,
+                                    reason=f"Banned by {ctx.author} ({ctx.author.id}). Reason : {reason}")
             except nextcord.Forbidden:
                 await ctx.send("Permissions manquantes :confused: (vérifiez la hiérarchie)")
             else:
@@ -45,10 +45,11 @@ class Ban(commands.Cog):
             if random.randint(1, 10) == 1:
                 if ctx.guild.id == "125723125685026816":
                     await ctx.author.send("https://discord.gg/E8qq6tN")
-                else :
+                else:
                     await ctx.author.send("https://discord.gg/jtntCqXz53")
                 try:
-                    await ctx.guild.kick(ctx.author, delete_message_days=0, reason=f"Banned by himself. Reason : {user} ({user.id}) used Uno Reverse card.")
+                    await ctx.guild.kick(ctx.author,
+                                         reason=f"Banned by himself. Reason : {user} ({user.id}) used Uno Reverse card.")
                 except nextcord.Forbidden:
                     await ctx.send("Permissions manquantes :confused: (vérifiez la hiérarchie)")
                 else:
@@ -57,7 +58,7 @@ class Ban(commands.Cog):
                     msg = await self.bot._(ctx.channel, f"ban.gunivers.selfban.{choice}")
                     await ctx.send(msg.format(ctx.author.mention, user.mention))
                     await ctx.send("https://thumbs.gfycat.com/BackInsignificantAfricanaugurbuzzard-size_restricted.gif")
-            
+
             # 1/10th chance of banning both banned and executor, Bothban event.
             if random.randint(1, 10) == 1:
                 if ctx.guild.id == "125723125685026816":
@@ -65,7 +66,7 @@ class Ban(commands.Cog):
                 else:
                     await user.send("https://discord.gg/jtntCqXz53")
                 try:
-                    await ctx.guild.kick(user, delete_message_days=0, reason=f"Banned by {ctx.author} ({ctx.author.id}). Reason : {reason}")
+                    await ctx.guild.kick(user, reason=f"Banned by {ctx.author} ({ctx.author.id}). Reason : {reason}")
                 except nextcord.Forbidden:
                     await ctx.send("Permissions manquantes :confused: (vérifiez la hiérarchie)")
                 else:
@@ -74,7 +75,8 @@ class Ban(commands.Cog):
                     else:
                         await ctx.author.send("https://discord.gg/jtntCqXz53")
                     try:
-                        await ctx.guild.kick(user, delete_message_days=0, reason=f"Banned by himself. Reason : {user} ({user.id}) banned him back.")
+                        await ctx.guild.kick(user,
+                                             reason=f"Banned by himself. Reason : {user} ({user.id}) banned him back.")
                     except nextcord.Forbidden:
                         # If there's an error when banning the author, we don't care and act like if a one-way ban happened.
                         choice = random.randint(0, 8)
@@ -86,15 +88,16 @@ class Ban(commands.Cog):
                         choice = random.randint(0, 3)
                         msg = await self.bot._(ctx.channel, f"ban.gunivers.bothban.{choice}")
                         await ctx.send(msg.format(ctx.author.mention, user.mention))
-                        await ctx.send("https://thumbs.gfycat.com/BackInsignificantAfricanaugurbuzzard-size_restricted.gif")
-
+                        await ctx.send(
+                            "https://thumbs.gfycat.com/BackInsignificantAfricanaugurbuzzard-size_restricted.gif")
 
             # 1/10th chance of rickrolling people, Rickroll event.
             if random.randint(1, 10) == 1:
-                msg = await self.bot._(ctx.channel, f"ban.gunivers.rickroll")
-                await ctx.send("Never gonna give you up,\nnever gonna let you down,\nnever gonna run around and ban you :musical_notes:")
+                await self.bot._(ctx.channel, f"ban.gunivers.rickroll")
+                await ctx.send(
+                    "Never gonna give you up,\nnever gonna let you down,\nnever gonna run around and ban you :musical_notes:")
                 await ctx.send("https://thumbs.gfycat.com/VengefulDesertedHalibut-size_restricted.gif")
-            
+
             # If ban is issued by Leirof, then Bald ban event.
             if ctx.author.id == "125722240896598016":
                 if ctx.guild.id == "125723125685026816":
@@ -102,7 +105,7 @@ class Ban(commands.Cog):
                 else:
                     await user.send("https://discord.gg/jtntCqXz53")
                 try:
-                    await ctx.guild.kick(user, delete_message_days=0, reason=f"Banned by {ctx.author} ({ctx.author.id}). Reason : {reason}")
+                    await ctx.guild.kick(user, reason=f"Banned by {ctx.author} ({ctx.author.id}). Reason : {reason}")
                 except nextcord.Forbidden:
                     await ctx.send("Permissions manquantes :confused: (vérifiez la hiérarchie)")
                 else:
@@ -110,7 +113,8 @@ class Ban(commands.Cog):
                     choice = random.randint(0, 8)
                     msg = await self.bot._(ctx.channel, f"ban.gunivers.ban.{choice}")
                     await ctx.send(msg.format(ctx.author.mention, user.mention))
-                    await ctx.send("https://media.discordapp.net/attachments/791335982666481675/979052868915064862/Chauve_qui_peut_.png")
+                    await ctx.send(
+                        "https://media.discordapp.net/attachments/791335982666481675/979052868915064862/Chauve_qui_peut_.png")
 
             # else, normal ban w/ random message
             else:
@@ -119,7 +123,7 @@ class Ban(commands.Cog):
                 else:
                     await user.send("https://discord.gg/jtntCqXz53")
                 try:
-                    await ctx.guild.kick(user, delete_message_days=0, reason=f"Banned by {ctx.author} ({ctx.author.id}). Reason : {reason}")
+                    await ctx.guild.kick(user, reason=f"Banned by {ctx.author} ({ctx.author.id}). Reason : {reason}")
                 except nextcord.Forbidden:
                     await ctx.send("Permissions manquantes :confused: (vérifiez la hiérarchie)")
                 else:
@@ -129,9 +133,9 @@ class Ban(commands.Cog):
                     await ctx.send(msg.format(ctx.author.mention, user.mention))
                     await ctx.send("https://thumbs.gfycat.com/PepperyEminentIndianspinyloach-size_restricted.gif")
 
-    #------------------#
+    # ------------------#
     # Commande /rban   #
-    #------------------#
+    # ------------------#
     # Parce qu'il peut être pratique de bannir tout de même
 
     @commands.command(name="rban")
@@ -150,7 +154,8 @@ class Ban(commands.Cog):
                 await ctx.send("Mon rôle n'est pas assez haut pour bannir cet individu :confused:")
                 return
             try:
-                await ctx.guild.ban(user, delete_message_days=0, reason=f"Banned by {ctx.author} ({ctx.author.id}). Reason : {reason}")
+                await ctx.guild.ban(user, delete_message_days=0,
+                                    reason=f"Banned by {ctx.author} ({ctx.author.id}). Reason : {reason}")
             except nextcord.Forbidden:
                 await ctx.send("Permissions manquantes :confused: (vérifiez la hiérarchie)")
             else:
