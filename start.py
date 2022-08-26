ee0a86d7dd91e364bc423433f5bfe9c2ace64379
Aeris1One <aeris@aeris-one.fr>
2022-08-26 11:50:17 +0200

    🌟refactor(start): start.py refactor (6.75 -> 10)

--- start.py
+++ start.py
@@ -1,8 +1,20 @@
 #!/usr/bin/env python
 # coding=utf-8
+"""
+Gipsy start functions
+"""
 
-import discord, time, asyncio, logging, json, sys, os, argparse
-from shutil import copyfile
+
+import argparse
+import asyncio
+import logging
+import os
+import sys
+import time
+import discord
+
+from bot.docs import generate_docs
+from bot.config import get_config
 from utils import Gunibot, setup_logger
 
 # check python version
@@ -23,31 +35,53 @@ for plugin in os.listdir('./plugins/'):
     if plugin[0] != '_':
         if os.path.isdir('./plugins/' + plugin):
             plugins.append("plugins." + plugin + '.bot.main')
-        
-# Generating docs
-from bot.docs import generate_docs
+
+# Generate docs
 generate_docs()
 
-#---------------#
-#    M A I N    #
-#---------------#
 
+def print_ascii_art():
+    """
+    Print GIPSY 2.0 ascii art
+    """
+    # Disable all the anomalous-backslash-in-string violations in this function
+    # pylint: disable=anomalous-backslash-in-string
+    print("""
+      ___  ____  ____  ___  _  _        ___     ___  
+     / __)(_  _)(  _ \/ __)( \/ )      (__ \   / _ \\ 
+    ( (_-. _)(_  )___/\__ \ \  /        / _/  ( (_) )
+     \___/(____)(__)  (___/ (__)       (____)()\___/ 
+
+        """)
+
+
+# ---------------#
+#    M A I N    #
+# ---------------#
 def main():
-    
+    """
+    Main function
+    """
     # Getting global config
-    from bot.config import get_config
-    conf = get_config('./config/config', isBotConfig = True)
-    if conf == None:
+    conf = get_config('./config/config', isBotConfig=True)
+    if conf is None:
         return 1
 
     # Getting plugins configs
-    for plugin in os.listdir('./plugins/'):
-        if plugin[0] != '_':
-            if os.path.isfile('./plugins/' + plugin + '/config/require-example.json'):
-                conf.update(get_config('./plugins/' + plugin + '/config/require', isBotConfig = False))
+    for plugin_dir in os.listdir('./plugins/'):
+        if plugin_dir[0] != '_':
+            if os.path.isfile('./plugins/' + plugin_dir + '/config/require-example.json'):
+                conf.update(
+                    get_config('./plugins/' + plugin_dir + '/config/require', isBotConfig=False)
+                )
 
     # Creating client
-    client = Gunibot(case_insensitive=True, status=discord.Status("online"), beta=False, config=conf)
+    client = Gunibot(
+        case_insensitive=True,
+        status=discord.Status("online"),
+        beta=False,
+        config=conf
+    )
 
     # Writing logs + welcome message
     if not os.path.isdir("logs"):
@@ -56,43 +90,38 @@ def main():
     log.setLevel(logging.DEBUG)
     log.info("Lancement du bot")
 
-    print("""
-  ___  ____  ____  ___  _  _        ___     ___  
- / __)(_  _)(  _ \/ __)( \/ )      (__ \   / _ \\ 
-( (_-. _)(_  )___/\__ \ \  /        / _/  ( (_) )
- \___/(____)(__)  (___/ (__)       (____)()\___/ 
-    
-    """)
+    # pylint: disable-next=anomalous-backslash-in-string
+    print_ascii_art()
 
-    # Loading extensions (global systems + plugins)
-    async def load(client, global_systems, plugins):
+    # Loading extensions (global system + plugins)
+    async def load(bot_client, global_system_list, plugin_list):
         loaded = 0
         failed = 0
         notloaded = ""
-        for extension in global_systems + plugins:
+        for extension in global_system_list + plugin_list:
             try:
-                await client.load_extension(extension)
+                await bot_client.load_extension(extension)
                 loaded += 1
-            except:
-                log.exception(f'\nFailed to load extension {extension}')
+            except Exception: # pylint: disable=broad-except
+                log.exception('Failed to load extension: %s', extension)
                 notloaded += "\n - " + extension
                 failed += 1
         if failed > 0:
-            raise Exception("\n{} modules not loaded".format(failed) + notloaded)
+            raise Exception(f"\n{failed} modules not loaded" + notloaded)
         return loaded, failed
 
     # Printing info when the bot is started
     async def on_ready():
         """Called when the bot is connected to Discord API"""
         print('Bot connecté')
-        print("Nom : "+client.user.name)
-        print("ID : "+str(client.user.id))
+        print("Nom : " + client.user.name)
+        print("ID : " + str(client.user.id))
         if len(client.guilds) < 200:
             serveurs = [x.name for x in client.guilds]
             print(
-                "Connecté sur ["+str(len(client.guilds))+"] "+", ".join(serveurs))
+                "Connecté sur [" + str(len(client.guilds)) + "] " + ", ".join(serveurs))
         else:
-            print("Connecté sur "+str(len(client.guilds))+" serveurs")
+            print("Connecté sur " + str(len(client.guilds)) + " serveurs")
         print(time.strftime("%d/%m  %H:%M:%S"))
         loaded, failed = await load(client, global_systems, plugins)
         print(f"{loaded} plugins chargés, {failed} plugins en erreur")
