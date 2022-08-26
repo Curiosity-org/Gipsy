67457ba454b2ca1f4d872dcf1072da18381cc436
Aeris1One <aeris@aeris-one.fr>
2022-08-26 12:01:22 +0200

    🌟refactor(start): pass through black formatter

--- start.py
+++ start.py
@@ -25,16 +25,16 @@ if py_version.major != 3 or py_version.minor < 9:
 
 # Getting global system list
 global_systems = []
-for system in os.listdir('./bot/utils/'):
-    if os.path.isfile('./bot/utils/' + system) and system[-3:] == '.py':
+for system in os.listdir("./bot/utils/"):
+    if os.path.isfile("./bot/utils/" + system) and system[-3:] == ".py":
         global_systems.append("bot.utils." + system[0:-3])
 
 # Getting plugin list
 plugins = []
-for plugin in os.listdir('./plugins/'):
-    if plugin[0] != '_':
-        if os.path.isdir('./plugins/' + plugin):
-            plugins.append("plugins." + plugin + '.bot.main')
+for plugin in os.listdir("./plugins/"):
+    if plugin[0] != "_":
+        if os.path.isdir("./plugins/" + plugin):
+            plugins.append("plugins." + plugin + ".bot.main")
 
 # Generate docs
 generate_docs()
@@ -44,15 +44,18 @@ def print_ascii_art():
     """
     Print GIPSY 2.0 ascii art
     """
-    # Disable all the anomalous-backslash-in-string violations in this function
+    # Disable some pylints warning violations in this function
     # pylint: disable=anomalous-backslash-in-string
-    print("""
+    # pylint: disable=trailing-whitespace
+    print(
+        """
       ___  ____  ____  ___  _  _        ___     ___  
      / __)(_  _)(  _ \/ __)( \/ )      (__ \   / _ \\ 
     ( (_-. _)(_  )___/\__ \ \  /        / _/  ( (_) )
      \___/(____)(__)  (___/ (__)       (____)()\___/ 
 
-        """)
+        """
+    )
 
 
 # ---------------#
@@ -63,24 +66,25 @@ def main():
     Main function
     """
     # Getting global config
-    conf = get_config('./config/config', isBotConfig=True)
+    conf = get_config("./config/config", isBotConfig=True)
     if conf is None:
         return 1
 
     # Getting plugins configs
-    for plugin_dir in os.listdir('./plugins/'):
-        if plugin_dir[0] != '_':
-            if os.path.isfile('./plugins/' + plugin_dir + '/config/require-example.json'):
+    for plugin_dir in os.listdir("./plugins/"):
+        if plugin_dir[0] != "_":
+            if os.path.isfile(
+                "./plugins/" + plugin_dir + "/config/require-example.json"
+            ):
                 conf.update(
-                    get_config('./plugins/' + plugin_dir + '/config/require', isBotConfig=False)
+                    get_config(
+                        "./plugins/" + plugin_dir + "/config/require", isBotConfig=False
+                    )
                 )
 
     # Creating client
     client = Gunibot(
-        case_insensitive=True,
-        status=discord.Status("online"),
-        beta=False,
-        config=conf
+        case_insensitive=True, status=discord.Status("online"), beta=False, config=conf
     )
 
     # Writing logs + welcome message
@@ -102,8 +106,8 @@ def main():
             try:
                 await bot_client.load_extension(extension)
                 loaded += 1
-            except Exception: # pylint: disable=broad-except
-                log.exception('Failed to load extension: %s', extension)
+            except Exception:  # pylint: disable=broad-except
+                log.exception("Failed to load extension: %s", extension)
                 notloaded += "\n - " + extension
                 failed += 1
         if failed > 0:
@@ -113,26 +117,29 @@ def main():
     # Printing info when the bot is started
     async def on_ready():
         """Called when the bot is connected to Discord API"""
-        print('Bot connecté')
+        print("Bot connecté")
         print("Nom : " + client.user.name)
         print("ID : " + str(client.user.id))
         if len(client.guilds) < 200:
             serveurs = [x.name for x in client.guilds]
             print(
-                "Connecté sur [" + str(len(client.guilds)) + "] " + ", ".join(serveurs))
+                "Connecté sur [" + str(len(client.guilds)) + "] " + ", ".join(serveurs)
+            )
         else:
             print("Connecté sur " + str(len(client.guilds)) + " serveurs")
         print(time.strftime("%d/%m  %H:%M:%S"))
         loaded, failed = await load(client, global_systems, plugins)
         print(f"{loaded} plugins chargés, {failed} plugins en erreur")
-        print('------')
+        print("------")
         await asyncio.sleep(2)
 
     client.add_listener(on_ready)
 
     # Check if the bot must run in beta
     parser = argparse.ArgumentParser()
-    parser.add_argument("-b", "--beta", help="Run with the beta bot token", action='store_true')
+    parser.add_argument(
+        "-b", "--beta", help="Run with the beta bot token", action="store_true"
+    )
     args = parser.parse_args()
 
     # Launch bot
