6a23823809bd431ce3ee217389672c62ee32bf2c
ascpial <mail@ascpial.fr>
2022-09-07 21:15:58 +0200

    🗑fix(ban): duplicated line

--- plugins/ban/events/bothban.py
+++ plugins/ban/events/bothban.py
@@ -12,28 +12,27 @@ async def execute(
     """Ban both the executor and the targeted user."""
     if await ban_plugin.fake_ban(ctx, user):
         if await ban_plugin.fake_ban(ctx, ctx.author):
-            if await ban_plugin.fake_ban(ctx, ctx.author):
-                # If there's no error, find a random message and send
-                # it.
-                choice = random.randint(0, 3)
-                msg = await ctx.bot._(
-                    ctx.channel, f"ban.gunivers.bothban.{choice}"
-                )
-                await ctx.send(msg.format(ctx.author.mention, user.mention))
-                await ctx.send(
-                    "https://thumbs.gfycat.com/BackInsignificantAfricanaugurbuzzard-size_restricted.gif"
-                )
-                return
-            else:
-                # we cannot ban the author, so we act as if it was a one-way ban
-                choice = random.randint(0, 9)
-                msg = await ctx.bot._(
-                    ctx.channel,
-                    f"ban.gunivers.ban.{choice}"
-                )
-                await ctx.send(
-                    msg.format(ctx.author.mention, user.mention)
-                )
-                await ctx.send(
-                    "https://thumbs.gfycat.com/PepperyEminentIndianspinyloach-size_restricted.gif"
-                )
+            # If there's no error, find a random message and send
+            # it.
+            choice = random.randint(0, 3)
+            msg = await ctx.bot._(
+                ctx.channel, f"ban.gunivers.bothban.{choice}"
+            )
+            await ctx.send(msg.format(ctx.author.mention, user.mention))
+            await ctx.send(
+                "https://thumbs.gfycat.com/BackInsignificantAfricanaugurbuzzard-size_restricted.gif"
+            )
+            return
+        else:
+            # we cannot ban the author, so we act as if it was a one-way ban
+            choice = random.randint(0, 9)
+            msg = await ctx.bot._(
+                ctx.channel,
+                f"ban.gunivers.ban.{choice}"
+            )
+            await ctx.send(
+                msg.format(ctx.author.mention, user.mention)
+            )
+            await ctx.send(
+                "https://thumbs.gfycat.com/PepperyEminentIndianspinyloach-size_restricted.gif"
+            )
