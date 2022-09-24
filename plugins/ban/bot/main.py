df73286691d12d429732ed349c2bcac03da8b33d
Aeris One <aeris@e.email>
2022-09-24 13:37:29 +0200

    🗣️ fix(ban): fix missing translation

--- plugins/ban/bot/main.py
+++ plugins/ban/bot/main.py
@@ -237,7 +237,7 @@ class Ban(commands.Cog):
             await ctx.guild.kick(user, reason=f"Auto-ban!")
         except discord.Forbidden:
             await ctx.send(
-                "Permissions manquantes :confused: (vérifiez la hiérarchie)"
+                await ctx.bot._(ctx, 'ban.gunivers.hierarchy')
             )
             await invite_message.edit(
                 content=await ctx.bot._(ctx, 'ban.gunivers.urbetter')
