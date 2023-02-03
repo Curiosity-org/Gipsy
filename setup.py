116b235913919d9f95981555fee84f3f9413d9aa
Vincent (Leirof) <vince.lrf@gmail.com>
2023-02-03 16:48:59 +0100

    Added licence header to python files

--- setup.py
+++ setup.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python
 # coding=utf-8
 
+"""
+Ce programme est régi par la licence CeCILL soumise au droit français et
+respectant les principes de diffusion des logiciels libres. Vous pouvez
+utiliser, modifier et/ou redistribuer ce programme sous les conditions
+de la licence CeCILL diffusée sur le site "http://www.cecill.info".
+"""
+
 import sys
 import os
 import pkg_resources
