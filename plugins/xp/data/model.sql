c870cbe9ab24eee6e1ee961d8844f7c2b40f62e2
Vincent (Leirof) <vince.lrf@gmail.com>
2023-02-03 17:20:39 +0100

    Updated YML files

--- plugins/xp/data/model.sql
+++ plugins/xp/data/model.sql
@@ -1,7 +1,7 @@
-# Ce programme est régi par la licence CeCILL soumise au droit français et
-# respectant les principes de diffusion des logiciels libres. Vous pouvez
-# utiliser, modifier et/ou redistribuer ce programme sous les conditions
-# de la licence CeCILL diffusée sur le site "http://www.cecill.info".
+-- Ce programme est régi par la licence CeCILL soumise au droit français et
+-- respectant les principes de diffusion des logiciels libres. Vous pouvez
+-- utiliser, modifier et/ou redistribuer ce programme sous les conditions
+-- de la licence CeCILL diffusée sur le site "http://www.cecill.info".
 
 CREATE TABLE IF NOT EXISTS `xp` (
   `guild` BIGINT,
