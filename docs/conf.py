28240e47fcb60dfff02a5fb5b22787373f9117b8
Vincent (Leirof) <vince.lrf@gmail.com>
2023-02-18 10:10:43 +0100

    📝 Interactive doc (linked with repo discussions)

--- docs/conf.py
+++ docs/conf.py
@@ -1,56 +1,3 @@
-######################
-# TocTree generation #
-######################
-
-import os
-import shutil
-
-def generate_plugin_doc():
-    # Saving index.rst file content
-    with open("index.rst", "r") as f:
-        before = []
-        after = []
-        started = False
-        ended = False
-        for line in f:
-            if not started:
-                before.append(line)
-            if line.startswith(".. Start plugins documentation"):
-                started = True
-            if line.startswith(".. End plugins documentation"):
-                ended = True
-            if ended:
-                after.append(line)
-
-    if os.path.isdir(f"plugins"):
-        shutil.rmtree(f"plugins")
-
-    with open("index.rst", "w+") as toctree:
-        for line in before:
-            toctree.write(line)
-
-        # Generating plugin toctree and moving pugin's doc files in the global docs folder
-        toctree.write("\n.. toctree::\n   :maxdepth: 1\n   :caption: User guide\n\n")
-        path = os.path.join("", os.pardir)
-        for plugin in os.listdir(f"{path}/plugins"):
-            if os.path.isdir(f"{path}/plugins/" + plugin + "/docs"):
-                for file in os.listdir(f"{path}/plugins/" + plugin + "/docs"):
-                    if file[-3:] == ".md" or file[-4:] == ".rst":
-                        if not os.path.isdir(f"/plugins/{plugin}/"):
-                            os.makedirs(f"plugins/{plugin}/")
-                        shutil.copyfile(
-                            f"{path}/plugins/{plugin}/docs/{file}",
-                            f"plugins/{plugin}/{file}",
-                        )
-                        toctree.write(f"   plugins/{plugin}/{file}\n")
-
-        toctree.write("\n")
-        for line in after:
-            toctree.write(line)
-
-generate_plugin_doc()
-
-
 # Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
@@ -139,3 +86,80 @@ myst_enable_extensions = [
     "substitution",
     "tasklist",
 ]
+
+#==============================================================================
+# Plugin doc extraction
+#==============================================================================
+
+import os
+import shutil
+
+def generate_plugin_doc():
+    # Saving index.rst file content
+    with open("index.rst", "r") as f:
+        before = []
+        after = []
+        started = False
+        ended = False
+        for line in f:
+            if not started:
+                before.append(line)
+            if line.startswith(".. Start plugins documentation"):
+                started = True
+            if line.startswith(".. End plugins documentation"):
+                ended = True
+            if ended:
+                after.append(line)
+
+    if os.path.isdir(f"plugins"):
+        shutil.rmtree(f"plugins")
+
+    with open("index.rst", "w+") as toctree:
+        for line in before:
+            toctree.write(line)
+
+        # Generating plugin toctree and moving pugin's doc files in the global docs folder
+        toctree.write("\n.. toctree::\n   :maxdepth: 1\n   :caption: User guide\n\n")
+        path = os.path.join("", os.pardir)
+        for plugin in os.listdir(f"{path}/plugins"):
+            if os.path.isdir(f"{path}/plugins/" + plugin + "/docs"):
+                for file in os.listdir(f"{path}/plugins/" + plugin + "/docs"):
+                    if file[-3:] == ".md" or file[-4:] == ".rst":
+                        if not os.path.isdir(f"/plugins/{plugin}/"):
+                            os.makedirs(f"plugins/{plugin}/")
+                        shutil.copyfile(
+                            f"{path}/plugins/{plugin}/docs/{file}",
+                            f"plugins/{plugin}/{file}",
+                        )
+                        toctree.write(f"   plugins/{plugin}/{file}\n")
+
+                        if "repository_url" in html_theme_options and html_theme_options["repository_url"].startswith("https://github.com/"):
+                            orga = html_theme_options["repository_url"].split("/")[3]
+                            repo = html_theme_options["repository_url"].split("/")[4]
+
+                            with open(f"plugins/{plugin}/{file}", "a", encoding="utf-8") as f:
+                                f.write(f"\n---\n\n")
+                                f.write(f"# 💬 Did it help you?\n\n")
+                                f.write(f"Feel free to leave your questions and feedbacks below!\n\n")
+                                f.write(f"<script src=\"https://giscus.app/client.js\"\n")
+                                f.write(f"        data-repo=\"{orga}/{repo}\"\n")
+                                f.write(f"        data-repo-id=\"R_kgDOHQph3g\"\n")
+                                f.write(f"        data-category=\"Documentation\"\n")
+                                f.write(f"        data-category-id=\"DIC_kwDOHQph3s4CUSnO\"\n")
+                                f.write(f"        data-mapping=\"title\"\n")
+                                f.write(f"        data-strict=\"0\"\n")
+                                f.write(f"        data-reactions-enabled=\"1\"\n")
+                                f.write(f"        data-emit-metadata=\"0\"\n")
+                                f.write(f"        data-input-position=\"bottom\"\n")
+                                f.write(f"        data-theme=\"light\"\n")
+                                f.write(f"        data-lang=\"fr\"\n")
+                                f.write(f"        data-loading=\"lazy\"\n")
+                                f.write(f"        crossorigin=\"anonymous\"\n")
+                                f.write(f"        async>\n")
+                                f.write(f"</script>\n")
+
+        toctree.write("\n")
+        for line in after:
+            toctree.write(line)
+
+generate_plugin_doc()
