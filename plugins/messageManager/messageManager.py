d14ac41cb36fcc40068b9ebc9e973af1408908df
Majoras16 <53090319+Majoras16@users.noreply.github.com>
2023-02-20 11:53:32 +0100

    Use the user's nickname instead of the username

--- plugins/messageManager/messageManager.py
+++ plugins/messageManager/messageManager.py
@@ -59,7 +59,7 @@ class MessageManager(commands.Cog):
 
         if (user is not None and text is not None): # c'est python, autant être verbeux
             # Create a webhook in the image of the targeted member
-            webhook = await ctx.channel.create_webhook(name=user.name)
+            webhook = await ctx.channel.create_webhook(name=user.display_name)
             await webhook.send(content=text, avatar_url=user.display_avatar)
 
             # Deletes the original message as well as the webhook
