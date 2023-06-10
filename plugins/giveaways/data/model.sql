5779ac679065453ee7bc6566cd4e9fa17f7dcfb7
Z Runner <z.runner.mc@gmail.com>
2023-06-10 18:33:22 -0400

    🗃️ refact(gaw): remove useless SQL column

--- plugins/giveaways/data/model.sql
+++ plugins/giveaways/data/model.sql
@@ -10,7 +10,6 @@ CREATE TABLE IF NOT EXISTS `giveaways` (
   `max_entries` INT DEFAULT 0,
   `ends_at` TIMESTAMP DEFAULT NULL,
   `message` BIGINT DEFAULT NULL,
-  `running` BOOLEAN DEFAULT 1,
   `users` BLOB NOT NULL
 );
 CREATE INDEX IF NOT EXISTS idx_giveaways_guild ON `giveaways` (`guild`);
