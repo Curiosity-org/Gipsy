db8781ceb067a4fae0ab9660be1de3ee817b0eca
Charles P <charles.pingard@viacesi.fr>
2023-11-14 16:01:35 +0100

    :sparkles: feat: add ping monitoring

--- plugins/monitoring/monitoring.py
+++ plugins/monitoring/monitoring.py
@@ -4,6 +4,7 @@ respectant les principes de diffusion des logiciels libres. Vous pouvez
 utiliser, modifier et/ou redistribuer ce programme sous les conditions
 de la licence CeCILL diffusée sur le site "http://www.cecill.info".
 """
+import time
 
 # pylint: disable=unused-import
 from discord.ext import tasks, commands
@@ -25,32 +26,40 @@ class Monitoring(commands.Cog):
         self.logger = core.setup_logger(self.file)
         self.config = core.config.get(self.file)
         if self.config["monitoring_enabled"]:
-            # test if the url is valid
-            try:
-                request = requests.get(self.config["monitoring_push_url"], timeout=5)
-                if (request.status_code != 200 or request.json()["ok"] != False):
-                    raise requests.exceptions.RequestException("URL returned an error")
-            except requests.exceptions.RequestException as e:
-                self.logger.error(f"Error while sending heartbeat to monitoring: {e}")
-                self.logger.warning(
-                    "Monitoring disabled for this session: an error occured while sending the heartbeat to the monitoring server.\n"
-                    "Error : %s",
-                    e,
-                )
-                return
-            #pylint: disable=no-member
-            self.ping_monitoring.start()
-            self.logger.info(
-                "Monitoring enabled successfully for %s", self.config["monitoring_push_url"]
-            )
+            for i in range(5):
+                if self.ping_monitoring():
+                    self.logger.info("Monitoring test ping successful")
+                    self.logger.info("Monitoring enabled")
+                    self.loop.start()
+                    return
+                else:
+                    self.logger.warning(f"Monitoring ping failed {i+1} times")
+                    time.sleep(5)
+            self.logger.error("Monitoring disabled due to ping failure")
+
+
+    def ping_monitoring(self):
+        # retrieve Discord Ping
+        ping = round(self.bot.latency*1000, 0)
+        # build URL
+        url = (self.config["monitoring_push_url"] +
+               self.config["monitoring_push_monitor"] +
+               "?status=up&msg=OK&ping=" + str(ping))
+        # send request
+        try:
+            request = requests.get(url, timeout=5)
+            if (request.status_code != 200 or request.json()["ok"] != True):
+                raise requests.exceptions.RequestException(request.json()["msg"])
+        except requests.exceptions.RequestException as e:
+            self.logger.error(f"Error while sending heartbeat to monitoring: {e}")
+            return False
+        return True
+
 
     @tasks.loop(seconds=20)
-    async def ping_monitoring(self):
-        try:
-            requests.get(self.config["monitoring_push_url"], timeout=5)
-        except requests.exceptions.RequestException as e:
-            self.logger.error("Error while sending heartbeat to monitoring: %s", e)
+    async def loop(self):
+        self.ping_monitoring()
 
-    @ping_monitoring.before_loop
+    @loop.before_loop
     async def before_ping_monitoring(self):
         await self.bot.wait_until_ready()
